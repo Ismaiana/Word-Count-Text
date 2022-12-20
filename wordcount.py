c06d39835b5a2afb784ecb88cb2ec73eebe25b54
Ismaiana Cruz De Lima <ismaiana_@hotmail.com>
2022-12-19 23:36:44 -0700

    Updated code

--- wordcount.py
+++ wordcount.py
@@ -1,42 +1,30 @@
 """Count words in file."""
-import sys
-# filename = test.txt
-# fp = open(test.txt)
 
 data = open("test.txt")
-
-
 
 
 dict_count = {}
 
 def word_count(data):
 
+
+
+    for line in data:
+
+        words = line.split(" ")
+
+        for word in words:
+
+
+            dict_count[word] = dict_count.get(word, 0) + 1
+
+    for word , value in dict_count.items():
+
+        print(f"{word} {value}")     
     
-   for line in data:
 
-        words = line.split()
-
-        for words in data:
-        
-            if words not in dict_count:
-            
-                dict_count[words] = 1
-
-            else:
-
-                dict_count[words] += 1
-            
-            
-        return dict_count
-
-print(word_count(data))
+word_count(data)
 
 
 
 
-
-
-# for loop to go over the lines 
-# we want the if stat to take each word and assign it as a key with val 1
-# we want anything else to check if there's a key and assign value 
